30f8ff83a7b05f8292ae28f9331e4bfb0d915db1
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-11-11 19:59:46 +0300

    Done 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    let argLength = arguments.length
+    arr = arguments[0]
 
+    for (let itemToRemoveIndex = 1; itemToRemoveIndex < argLength; itemToRemoveIndex++){
+        let itemToRemove = arguments[itemToRemoveIndex]
+        let itemToRemoveIndexInArr = arr.indexOf(itemToRemove)
+        if (itemToRemoveIndexInArr > -1){
+            arr.splice(itemToRemoveIndexInArr, 1)
+        }
+
+    }
+    return arr
 };
 
 // Do not edit below this line
